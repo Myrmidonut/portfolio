1f58e6b77c96343a9b0909ff8971850765225530
Frederik Funke <frederik-funke@gmx.de>
2019-02-13 20:02:50 +0100

    initial commit, basic navbar

---
+++ script.js
@@ -0,0 +1,14 @@
+const burger = document.querySelector("#burger")
+const navBottom = document.querySelector("#nav-bottom")
+const burgerMenuLink = document.querySelectorAll(".burger-menu-link");
+
+burger.addEventListener("click", e => {
+  e.preventDefault()
+  navBottom.classList.toggle("nav-bottom-hide")
+})
+
+burgerMenuLink.forEach(e => {
+  e.addEventListener("click", e => {
+    navBottom.classList.toggle("nav-bottom-hide")
+  })
+})
