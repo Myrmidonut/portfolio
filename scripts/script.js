d8ba81af6bfeba2dab77611daebe8796ef91d6e2
Frederik Funke <frederik-funke@gmx.de>
2019-02-23 16:57:14 +0100

    font and bars aligned by text width

--- scripts/script.js
+++ scripts/script.js
@@ -16,59 +16,69 @@ burgerMenuLink.forEach(e => {
 })
 
 // CANVAS
-const canvas = document.querySelector("canvas")
-
-canvas.width = document.querySelector("body").scrollWidth
-canvas.height = window.innerHeight
-
-const centerX = canvas.width / 2
-const centerY = canvas.height / 2 - 50;
-const heightThird = canvas.height / 100 * 70
-const heightHalf = canvas.height / 2 - 50
-const radius = Math.sqrt(Math.pow(canvas.height, 2) + Math.pow(canvas.width, 2))
-
-const rays = canvas.getContext("2d")
-
-rays.beginPath();
-
-for (let degree = 0; degree < 360; degree += 2) {
-  if (degree >= 90 && degree <= 270) {
-    const rand = degree * Math.PI / 180
-
-    rays.moveTo(centerX, heightThird + 50)
-    rays.lineTo(centerX + radius * Math.sin(rand), heightThird + 50 + radius * Math.cos(rand))
-  }
-}
-
-const gradient = rays.createLinearGradient(0, 0, 0, heightThird)
-
-gradient.addColorStop(0, "#b37f01")
-gradient.addColorStop(1, "#1b1b1b")
-
-rays.lineWidth = 2
-rays.strokeStyle = gradient
-
-rays.stroke()
-
 const fontLoad = new FontFace("Grenadier", "url(fonts/GrenadierNF.ttf)")
 
 fontLoad.load().then(() => {
-  const title = canvas.getContext("2d")
-  const titleText = "FREDERIK"
+  const canvas = document.querySelector("canvas")
 
-  title.font = `100 ${canvas.height / 7.5}px Grenadier`
+  canvas.width = document.querySelector("body").scrollWidth
+  canvas.height = window.innerHeight
+
+  const centerX = canvas.width / 2
+  const centerY = canvas.height / 2 - 50;
+  const heightThird = canvas.height / 100 * 70
+  const heightHalf = canvas.height / 2 - 50
+  const radius = Math.sqrt(Math.pow(canvas.height, 2) + Math.pow(canvas.width, 2))
+
+  const rays = canvas.getContext("2d")
+
+  rays.beginPath();
+
+  for (let degree = 0; degree < 360; degree += 2) {
+    if (degree >= 90 && degree <= 270) {
+      const rand = degree * Math.PI / 180
+
+      rays.moveTo(centerX, heightThird + 50)
+      rays.lineTo(centerX + radius * Math.sin(rand), heightThird + 50 + radius * Math.cos(rand))
+    }
+  }
+
+  const gradient = rays.createLinearGradient(0, 0, 0, heightThird)
+
+  gradient.addColorStop(0, "#b37f01")
+  gradient.addColorStop(1, "#1b1b1b")
+
+  rays.lineWidth = 2
+  rays.strokeStyle = gradient
+
+  rays.stroke()
+
+  const title = canvas.getContext("2d")
+  const titleText = "frederik"
+
+  if (canvas.width <= 700) {
+    title.font = `100 ${canvas.width / 6}px Grenadier`
+  } else {
+    title.font = `100 ${canvas.height / 6.5}px Grenadier`
+  }
+
   title.textBaseline = "middle"
   title.fillStyle = "white"
   title.textAlign = "center"
 
   const titleWidth = title.measureText(titleText).width
 
-  title.fillText("FREDERIK", centerX, heightHalf)
+  title.fillText("frederik", centerX, heightHalf)
 
   const subtitle = canvas.getContext("2d")
   const subtitleText = "developer"
 
-  subtitle.font = `100 ${canvas.height / 10.3}px Grenadier`
+  if (canvas.width <= 700) {
+    subtitle.font = `100 ${canvas.width / 10.3}px Grenadier`
+  } else {
+    subtitle.font = `100 ${canvas.height / 15}px Grenadier`
+  }
+
   subtitle.textBaseline = "middle"
   subtitle.fillStyle = "white"
   subtitle.textAlign = "center"
@@ -82,32 +92,32 @@ fontLoad.load().then(() => {
   horizontalBars.beginPath();
 
   // frederik
-  horizontalBars.moveTo(centerX, heightHalf * 0.8)
-  horizontalBars.lineTo(centerX + titleWidth / 2 + 15, heightHalf * 0.8)
-  horizontalBars.moveTo(centerX, heightHalf * 0.8)
-  horizontalBars.lineTo(centerX - titleWidth / 2 - 15, heightHalf * 0.8)
+  horizontalBars.moveTo(centerX, heightHalf - titleWidth / 7)
+  horizontalBars.lineTo(centerX + titleWidth / 2 + 15, heightHalf - titleWidth / 7)
+  horizontalBars.moveTo(centerX, heightHalf - titleWidth / 7)
+  horizontalBars.lineTo(centerX - titleWidth / 2 - 15, heightHalf - titleWidth / 7)
 
-  horizontalBars.moveTo(centerX, heightHalf * 1.15)
-  horizontalBars.lineTo(centerX + titleWidth / 2 + 15, heightHalf * 1.15)
-  horizontalBars.moveTo(centerX, heightHalf * 1.15)
-  horizontalBars.lineTo(centerX - titleWidth / 2 - 15, heightHalf * 1.15)
+  horizontalBars.moveTo(centerX, heightHalf + titleWidth / 7)
+  horizontalBars.lineTo(centerX + titleWidth / 2 + 15, heightHalf + titleWidth / 7)
+  horizontalBars.moveTo(centerX, heightHalf + titleWidth / 7)
+  horizontalBars.lineTo(centerX - titleWidth / 2 - 15, heightHalf + titleWidth / 7)
 
   // developer
   horizontalBars.moveTo(centerX + subtitleWidth / 2 + 10, heightThird)
   horizontalBars.lineTo(canvas.width - 10, heightThird)
-  horizontalBars.moveTo(centerX + subtitleWidth / 2 + 10, heightThird * 0.96)
-  horizontalBars.lineTo(canvas.width - 10, heightThird * 0.96)
-  horizontalBars.moveTo(centerX + subtitleWidth / 2 + 10, heightThird * 1.04)
-  horizontalBars.lineTo(canvas.width - 10, heightThird * 1.04)
+  horizontalBars.moveTo(centerX + subtitleWidth / 2 + 10, heightThird - subtitleWidth / 15)
+  horizontalBars.lineTo(canvas.width - 10, heightThird - subtitleWidth / 15)
+  horizontalBars.moveTo(centerX + subtitleWidth / 2 + 10, heightThird + subtitleWidth / 15)
+  horizontalBars.lineTo(canvas.width - 10, heightThird + subtitleWidth / 15)
 
   horizontalBars.moveTo(centerX - subtitleWidth / 2 - 10, heightThird)
   horizontalBars.lineTo(10, heightThird)
-  horizontalBars.moveTo(centerX - subtitleWidth / 2 - 10, heightThird * 0.96)
-  horizontalBars.lineTo(10, heightThird * 0.96)
-  horizontalBars.moveTo(centerX - subtitleWidth / 2 - 10, heightThird * 1.04)
-  horizontalBars.lineTo(10, heightThird * 1.04)
+  horizontalBars.moveTo(centerX - subtitleWidth / 2 - 10, heightThird - subtitleWidth / 15)
+  horizontalBars.lineTo(10, heightThird - subtitleWidth / 15)
+  horizontalBars.moveTo(centerX - subtitleWidth / 2 - 10, heightThird + subtitleWidth / 15)
+  horizontalBars.lineTo(10, heightThird + subtitleWidth / 15)
 
-  horizontalBars.lineWidth = 7
+  horizontalBars.lineWidth = 5
   horizontalBars.strokeStyle = "#ce8f00"
 
   horizontalBars.stroke()
